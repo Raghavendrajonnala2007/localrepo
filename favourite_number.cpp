a2aba2cf937a94df83fb39b6a1fca75aed725a67
Raghavendra jonnala <jonnala.raghavendra113@gmail.com>
2025-06-20 22:30:29 +0530

    New project

---
+++ favourite_number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::cout <<"Enter a number: ";
+    int a;
+    std::cin>>a;
+    std::cout <<a;
+    std::cout<<"Wow that is my favourite number too!! "<<a<<"\n";
+    return 0;
+}
