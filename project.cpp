2012540f6ccce7ab2ccfdb46c64b65bc1d571a4b
Raghavendra jonnala <jonnala.raghavendra113@gmail.com>
2025-06-20 20:45:22 +0530

    Circumfurence project

--- project.cpp
+++ project.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-
+const double PI = 3.14;
 int main(){
-    std::string a;
-    std::cout << "Please enter your name: ";
-    std::cin >> a;
+    double r;
+    std::cout<<"Enter the radius: ";
+    std::cin>>r;
+    std::cout<<"The radius is: ";
+    double c = 2*r*PI;
+    std::cout<<"circumfurence is: "<<c;
     return 0;
 }
